145a22ab5d75aafee85801546d9d049d704c915e
NumanAvci <78160036+NumanAvci@users.noreply.github.com>
2023-08-21 11:33:33 +0300

    StartScene edited

---
+++ Assets/Scripts/SceneInfo.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneInfo", menuName = "SceneConfigurations")]
+public class SceneInfo : ScriptableObject
+{
+    public int _playerType;
+}
