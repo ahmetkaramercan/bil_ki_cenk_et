dc9d8fbb6b217286a1a83e13904fbf934b000be0
Mehmet-Safa <78314348+Mehmet-Safa@users.noreply.github.com>
2023-08-21 13:46:39 +0300

    Wrong and Correct texts added

--- Assets/Scripts/CorrectAnswer.cs
+++ Assets/Scripts/CorrectAnswer.cs
@@ -2,71 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CorrectAnswer : MonoBehaviour
 {
     public Text text;
     [SerializeField]
     private GameObject[] nextEnemy = new GameObject[1];
+
+    [SerializeField]
+    private TextMeshProUGUI _correctText;
+    
+    void Start(){
+        _correctText.SetText("");
+    }
+
     void OnTriggerEnter(Collider other)
     {
 
-        if(other.gameObject.tag == "Player"){
-            print("SAFAAAAAAgg");  // buraya adamın degmesi değil de okların degmesi olacak sekilde ayarla boylece sadece atılan seylerle isaretlenebilsin
+        if(other.gameObject.tag == "Arrow"){
+            _correctText.SetText("Correct !!!");
+            Invoke("Start",2);
+            nextEnemy[0].SetActive(true);
+            if ( gameObject.name.Equals("Ice C"))
+            {            
+                GameObject targetObject = GameObject.Find("Ice Question");
+                targetObject.SetActive(false);
+               
+            }
+            else if ( gameObject.name.Equals("Explosion D"))
+            {      
+                GameObject targetObject = GameObject.Find("Explosion Question");
+                targetObject.SetActive(false);
+                
+            }
+            else if ( gameObject.name.Equals("Poison B"))
+            {
+                GameObject targetObject = GameObject.Find("Poison Question");
+                targetObject.SetActive(false);
+            }
+            else if ( gameObject.name.Equals("Fire A"))
+            {     
+                GameObject targetObject = GameObject.Find("Fire Question");
+                targetObject.SetActive(false);
+            }
         }
-        nextEnemy[0].SetActive(true);
-        if ( gameObject.name.Equals("Ice C"))
-        {            
-            GameObject targetObject0 = GameObject.Find("Ice Question");
-            GameObject targetObject1 = GameObject.Find("Ice A");
-            GameObject targetObject2 = GameObject.Find("Ice B");
-            GameObject targetObject3 = GameObject.Find("Ice C");
-            GameObject targetObject4 = GameObject.Find("Ice D");
-            targetObject0.SetActive(false);
-            targetObject1.SetActive(false);
-            targetObject2.SetActive(false);
-            targetObject3.SetActive(false);
-            targetObject4.SetActive(false);
-        }
-        else if ( gameObject.name.Equals("Explosion D"))
-        {      
-            GameObject targetObject0 = GameObject.Find("Explosion Question");
-            GameObject targetObject1 = GameObject.Find("Explosion A");
-            GameObject targetObject2 = GameObject.Find("Explosion B");
-            GameObject targetObject3 = GameObject.Find("Explosion C");
-            GameObject targetObject4 = GameObject.Find("Explosion D");
-            targetObject0.SetActive(false);
-            targetObject1.SetActive(false);
-            targetObject2.SetActive(false);
-            targetObject3.SetActive(false);
-            targetObject4.SetActive(false);
-        }
-        else if ( gameObject.name.Equals("Poison B"))
-        {
-            GameObject targetObject0 = GameObject.Find("Poison Question");
-            GameObject targetObject1 = GameObject.Find("Poison A");
-            GameObject targetObject2 = GameObject.Find("Poison B");
-            GameObject targetObject3 = GameObject.Find("Poison C");
-            GameObject targetObject4 = GameObject.Find("Poison D");
-            targetObject0.SetActive(false);
-            targetObject1.SetActive(false);
-            targetObject2.SetActive(false);
-            targetObject3.SetActive(false);
-            targetObject4.SetActive(false);
-        }
-        else if ( gameObject.name.Equals("Fire A"))
-        {     
-            GameObject targetObject0 = GameObject.Find("Fire Question");
-            GameObject targetObject1 = GameObject.Find("Fire A");
-            GameObject targetObject2 = GameObject.Find("Fire B");
-            GameObject targetObject3 = GameObject.Find("Fire C");
-            GameObject targetObject4 = GameObject.Find("Fire D");
-            targetObject0.SetActive(false);
-            targetObject1.SetActive(false);
-            targetObject2.SetActive(false);
-            targetObject3.SetActive(false);
-            targetObject4.SetActive(false);
-        }
+        
 
     }
+    
 }
--- Assets/Scripts/WrongAnswer.cs
+++ Assets/Scripts/WrongAnswer.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WrongAnswer : MonoBehaviour
 {
-   void OnTriggerEnter(Collider other)
+    [SerializeField]
+    private TextMeshProUGUI _wrongText;
+
+    void Start(){
+        _wrongText.SetText("");
+    }
+
+    void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player"){
-            print("WRONGGGGG"); // bunu da ayni sekilde sadece atilan oklarla falan olacak sekilde ayarla
+        if(other.gameObject.tag == "Arrow"){
+            _wrongText.SetText("Wrong !!!");
+            Invoke("Start",2);
+            print("WRONG"); // bunu da ayni sekilde sadece atilan oklarla falan olacak sekilde ayarla
         }
-        print("WRONG");
+        //print("WRONG");
         //Debug.Log("Bu script " + gameObject.name + " objesine bağlıdır.");
 
     }
